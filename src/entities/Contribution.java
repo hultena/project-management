37581e416a4366d8f9ed35b9be8c7be1697b7d89
Alexander Hultén <gusalexhu@student.gu.se>
2018-12-19 15:59:44 +0100

    Contribution class

---
+++ src/entities/Contribution.java
@@ -0,0 +1,11 @@
+package entities;
+
+public class Contribution {
+    private String id;
+    private int time;
+
+    public Contribution(String id, int time){
+        this.id = id;
+        this.time = time;
+    }
+}
