e6cceb4bcd080d440e9422958c943ea5d5444bd8
Alexander Hultén <gusalexhu@student.gu.se>
2018-12-19 15:51:01 +0100

    Update src/entities/Task.java

--- src/entities/Task.java
+++ src/entities/Task.java
@@ -1,12 +1,15 @@
 package entities;
+import java.util.*;
 
 public class Task {
     private String name;
     private int budgetedHours;
-    
+    private List<Contribution> contributions;
+
     public Task(String name, int budgetedHours) {
         this.name = name;
         this.budgetedHours = budgetedHours;
+        contributions = new ArrayList<Contribution>();
     }
 
     public String getName() {
@@ -14,5 +17,12 @@ public class Task {
     }
     public int getBudgetedHours() {
         return this.budgetedHours;
+    }
+    public void addContribution(String id, int time){
+        Contribution newContribution = new Contribution(id, time);
+        contributions.add(newContribution);
+    }
+    public List<Contribution> getContributions(){
+        return contributions;
     }
 }
