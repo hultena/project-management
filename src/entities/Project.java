1574d2506d66d584fcaac969fef293c67953edfc
kalle hålldén <kallehalldeen@gmail.com>
2018-12-20 18:15:11 +0100

    Added more instance variables to Project and methods for adding and removing meber

--- src/entities/Project.java
+++ src/entities/Project.java
@@ -6,6 +6,8 @@ import java.util.*;
 public class Project {
     public List<Risk> riskMatrix;
     public List<Task> tasks;
+    public List<Member> members;
+    public int projectDuration;
 
     public Project() {
         this.riskMatrix = Parser.loadRisks();
@@ -14,6 +16,41 @@ public class Project {
     public void addTask(String name, int budgetedHours){
         Task newTask = new Task(name,budgetedHours);
         tasks.add(newTask);
+    }
+
+    public void setProjectDuration(int projectDurationWeeks) {
+        this.projectDuration = projectDurationWeeks;
+    }
+
+    public List<Member> getMembers() {
+        return this.members;
+    }
+
+    public void addMember(String name, String id) {
+        if (this.findMember(id) == null) {
+            this.members.add(new Member(name, id));
+        } else {
+            System.out.println("A member already exists with the ID: " + id);
+        }
+    }
+
+    public void removeMember(String id) {
+        Member memberToRemove = this.findMember(id);
+        if (memberToRemove != null) {
+            this.members.remove(memberToRemove);
+        } else {
+            System.out.println("No member exists with the ID: " + id);
+        }
+    }
+
+    public Member findMember(String id) {
+        Member foundMember;
+        for (let i = 0; i < members.length; i++) {
+            if (id.equals(members.get(i).getId())) {
+                foundMember = members.get(i);
+            }
+        }
+        return foundMember;
     }
 
 }
