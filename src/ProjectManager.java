9e4a7069c778e7ee5bd2f60e20c4a4cf070a61ae
bartekspitza <bartekspitza@hotmail.com>
2018-12-20 18:02:34 +0100

    Adjusted JSON file

--- src/ProjectManager.java
+++ src/ProjectManager.java
@@ -4,11 +4,11 @@ public class ProjectManager {
 
     public static void main(String[] args) throws Exception {
 
+        // Following block should give the Parser the path of the json file
         if (args.length > 0) { // prevents crash when executed without command line arguments
             Parser.filename = args[0];
         }
 
-        Parser.loadJSON();
 
     }
 }
