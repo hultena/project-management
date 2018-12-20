96468be0d9a8bcee352e78ac029e5253fde2e67d
kalle hålldén <kallehalldeen@gmail.com>
2018-12-20 17:43:20 +0100

    added main folder and main method

---
+++ src/main/Program.java
@@ -0,0 +1,10 @@
+package main;
+
+import entities.*;
+
+public class Program {
+    public static void main(String[] args) {
+        Project buildSoftware = new Project();
+        System.out.println("Starting");
+    }
+}
