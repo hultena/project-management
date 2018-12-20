1aea51aeaf7430516ee0574953ca35f01d016492
bartekspitza <bartekspitza@hotmail.com>
2018-12-20 17:37:06 +0100

    Added additional jackson libs and json can now be read

--- src/ProjectManager.java
+++ src/ProjectManager.java
@@ -2,11 +2,13 @@ import utils.Parser;
 
 public class ProjectManager {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         if (args.length > 0) { // prevents crash when executed without command line arguments
             Parser.filename = args[0];
         }
+
+        Parser.loadJSON();
 
     }
 }
