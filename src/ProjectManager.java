1a82ed14559c7b1fe76d57f1cb671b556cd19f64
bartekspitza <bartekspitza@hotmail.com>
2018-12-20 16:58:28 +0100

    Added jackson lib and began on Parser

--- src/ProjectManager.java
+++ src/ProjectManager.java
@@ -1,8 +1,12 @@
-
+import utils.Parser;
 
 public class ProjectManager {
 
     public static void main(String[] args) {
-        // Entry to the program
+
+        if (args.length > 0) { // prevents crash when executed without command line arguments
+            Parser.filename = args[0];
+        }
+
     }
 }
