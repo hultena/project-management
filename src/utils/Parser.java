131243c230f8e41d0094ec9f540be94dec3a0a3a
bartekspitza <bartekspitza@hotmail.com>
2018-12-20 17:37:06 +0100

    Added additional jackson libs and json can now be read

--- src/utils/Parser.java
+++ src/utils/Parser.java
@@ -2,11 +2,28 @@ package utils;
 
 
 import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import entities.Risk;
-import com.fasterxml.jackson.*;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Parser {
     public static String filename;
+
+    public static void loadJSON() throws Exception {
+        filename = "/home/maikzy/gitreps/project-management/template.json";
+        byte[] jsonData = Files.readAllBytes(Paths.get(filename));
+        Map<String,String> myMap = new HashMap<String, String>();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(jsonData);
+
+        System.out.println(rootNode.get("tasks").get(1));
+    }
 
     public static List<Risk> loadRisks() {
         // TODO: Implement real parsing of JSON data
