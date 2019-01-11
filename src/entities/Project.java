21e4a029d70175319e12a2cc748ce5125d7a3fb2
hultena <44065966+hultena@users.noreply.github.com>
2019-01-11 17:04:57 +0100

    Added risk interactivity.

--- src/entities/Project.java
+++ src/entities/Project.java
@@ -30,6 +30,13 @@ public class Project {
         newTask.setEndWeek(endWeek);
         tasks.add(newTask);
     }
+    public void addRisk(String name,int severity,int probability){
+        Risk newRisk = new Risk();
+        newRisk.setName(name);
+        newRisk.setSeverity(severity);
+        newRisk.setProbability(probability);
+        riskMatrix.add(newRisk);
+    }
 
     public void setProjectDuration(int projectDurationWeeks) {
         this.projectDuration = projectDurationWeeks;
@@ -71,93 +78,94 @@ public class Project {
         return foundMember;
     }
 
+
 	/*Calculating the mean percentage of the tasks completed to
 	get an estimate for the entire project*/
 
-	public double meanPercentage(){
-	double totalPercent = 0;
-	int numberOfTasks = 0;
-	double meanPercentage = 0;
-	for (Task task : tasks) {
-			totalPercent = totalPercent + task.getCompletion();
-			numberOfTasks += 1;
+    public double meanPercentage(){
+        double totalPercent = 0;
+        int numberOfTasks = 0;
+        double meanPercentage = 0;
+        for (Task task : tasks) {
+            totalPercent = totalPercent + task.getCompletion();
+            numberOfTasks += 1;
         }
-		if (numberOfTasks != 0) {
-		meanPercentage = totalPercent/numberOfTasks;
-		}
-		return meanPercentage;
-	}
+        if (numberOfTasks != 0) {
+            meanPercentage = totalPercent/numberOfTasks;
+        }
+        return meanPercentage;
+    }
 
-	//Calculating the earned value with the mean percentage for the tasks.
+    //Calculating the earned value with the mean percentage for the tasks.
 
-	public double calculateEV(){
-	double EV = 0;
-	double completedPercent = meanPercentage();
-	double BAC = costOfPerformed();
+    public double calculateEV(){
+        double EV = 0;
+        double completedPercent = meanPercentage();
+        double BAC = costOfPerformed();
 
-	if(BAC != 0){
-	EV = completedPercent/BAC;	
-	}
-		return EV;
-	}
+        if(BAC != 0){
+            EV = completedPercent/BAC;
+        }
+        return EV;
+    }
 
 
-	//Calculating the total cost of the work performed for the tasks.
+    //Calculating the total cost of the work performed for the tasks.
 
-	public double costOfPerformed(){
-		int hoursSpent = 0;
-		double actualTotalPay = 0;
-		for(Task task : tasks) {
-			hoursSpent = hoursSpent + task.getTimeSpent();
-		}
-		actualTotalPay = hoursSpent * engineerSalary;
-		return actualTotalPay;
-	}
+    public double costOfPerformed(){
+        int hoursSpent = 0;
+        double actualTotalPay = 0;
+        for(Task task : tasks) {
+            hoursSpent = hoursSpent + task.getTimeSpent();
+        }
+        actualTotalPay = hoursSpent * engineerSalary;
+        return actualTotalPay;
+    }
 
-	//Calculating the scheduled total cost of the tasks.
+    //Calculating the scheduled total cost of the tasks.
 
-	public double costOfScheduled(){
-		double plannedTotalPay = 0;
-		int totalHoursPlanned = 0;
+    public double costOfScheduled(){
+        double plannedTotalPay = 0;
+        int totalHoursPlanned = 0;
 
-		for(Task task: tasks) {
-			totalHoursPlanned = totalHoursPlanned + task.getBudgetedHours();
-		}
-		plannedTotalPay = totalHoursPlanned * engineerSalary;
-		return plannedTotalPay;
-	}
+        for(Task task: tasks) {
+            totalHoursPlanned = totalHoursPlanned + task.getBudgetedHours();
+        }
+        plannedTotalPay = totalHoursPlanned * engineerSalary;
+        return plannedTotalPay;
+    }
 
-	//Calculating the schedule variance.
+    //Calculating the schedule variance.
 
-	public double calculateSV(){
-		double scheduleVariance = 0;
-		double workPerformed = costOfPerformed();
-		double workScheduled = costOfScheduled();
-		scheduleVariance = workPerformed - workScheduled;
-		return scheduleVariance;
-	}
+    public double calculateSV(){
+        double scheduleVariance = 0;
+        double workPerformed = costOfPerformed();
+        double workScheduled = costOfScheduled();
+        scheduleVariance = workPerformed - workScheduled;
+        return scheduleVariance;
+    }
 
-	//Calculating the cost variance.
+    //Calculating the cost variance.
 
-	public double calculateCV(){
-		double costVariance = 0;
-		double actualCost = costOfPerformed();
-		double budgetedCost = costOfScheduled();
-		costVariance = budgetedCost - actualCost;
-		return costVariance;
-	}
+    public double calculateCV(){
+        double costVariance = 0;
+        double actualCost = costOfPerformed();
+        double budgetedCost = costOfScheduled();
+        costVariance = budgetedCost - actualCost;
+        return costVariance;
+    }
 
-	//Calculating the project duration.
+    //Calculating the project duration.
 
-	public int calculateDuration(){
-	int duration = 0;
-	if(startWeek < endWeek){
-		duration = endWeek - startWeek;
-	} else if(startWeek > endWeek){
-		duration = (endWeek - startWeek) + 52;
-	} 
-	return duration;
-	}
+    public int calculateDuration(){
+        int duration = 0;
+        if(startWeek < endWeek){
+            duration = endWeek - startWeek;
+        } else if(startWeek > endWeek){
+            duration = (endWeek - startWeek) + 52;
+        }
+        return duration;
+    }
 
 
     public void setEndWeek(int endWeek) {
